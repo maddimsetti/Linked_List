42e07044fe1c8c877bf415631747193b1995c89e
prudhvi <prudhvieethakota123@gmail.com>
2021-05-25 02:06:13 +0530

    Added: Code for Creating Stack with data in sequence

--- main/java/com/linkedlist/Stack.java
+++ main/java/com/linkedlist/Stack.java
@@ -1,2 +1,48 @@
-package com.linkedlist;public class Stack {
+package com.linkedlist;
+
+/**
+ * @description create Class for Defining the Stack File in Data Structure
+ *
+ */
+public class Stack {
+    //Variables
+    private final MyLinkedList myLinkedList;
+
+    /**
+     * @description create Constructor for Initializing the Variables. Reuse the myLinkedList for Constructor
+     *
+     */
+    public Stack() {
+        this.myLinkedList = new MyLinkedList();
+    }
+
+    /**
+     * @description create method for pushing the Nodes into the list Reusing LinkedList Add method
+     *
+     */
+    public void push(INode myNode) {
+        myLinkedList.add(myNode);
+    }
+
+    /**
+     * @description create method for printing the Nodes from the list Reusing LinkedList Printing MyNode method
+     *
+     */
+    public void printStack() {
+        myLinkedList.printMyNode();
+    }
+    /**
+     * @description create method for Peeking the Nodes from the list, Reusing LinkedList Peeking MyNode method
+     *
+     */
+    public INode peek() {
+        return myLinkedList.head;
+    }
+    /**
+     * @description create method for Deleting the Nodes from the list, Reusing LinkedList Printing MyNode method
+     *
+     */
+    public INode pop() {
+        return myLinkedList.pop();
+    }
 }
--- test/java/com/linkedlist/StackTest.java
+++ test/java/com/linkedlist/StackTest.java
@@ -1,2 +1,29 @@
-package com.linkedlist;public class StackTest {
+package com.linkedlist;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @description create Class for Defining the Testing Stack List
+ *
+ */
+public class StackTest {
+    /**
+     * @description create method to Create a Stack of Sequence 56->30->70
+     *    Here So 70 will be added first then 30 and then 56 to make 56 on top of the Stack
+     */
+    @Test
+    public void given3NumbersWhenAddedToStackShouldHaveLastAddedNode() {
+        MyNode<Integer> myFirstNode = new MyNode(70);
+        MyNode<Integer> mySecondNode = new MyNode(30);
+        MyNode<Integer> myThirdNode = new MyNode(56);
+        Stack stack = new Stack();
+        stack.push(myFirstNode);     //Adding FirstNode to the list Using Stack
+        stack.push(mySecondNode);    ////Adding SecondNode to the list Using stack
+        stack.push(myThirdNode);     //Adding ThirdNode to the list Using stack
+        stack.printStack();          //Printing the Stack
+        INode peek = stack.peek();
+        Assertions.assertEquals(myThirdNode,peek);   //Testing the Peek Point
+    }
 }
+
